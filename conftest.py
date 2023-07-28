6b737fc40264890c3996c29ab707bc873614bf29
AiM-HyperJump <140639363+AiM-HyperJump@users.noreply.github.com>
2023-07-29 00:38:06 +0500

    add file conftest.py (Stepik task 3.6.10)

---
+++ conftest.py
@@ -0,0 +1,24 @@
+#conftest for Stepik in Git
+
+import pytest
+from selenium import webdriver
+
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', 
+                     action='store', 
+                     default=None, 
+                     help="Choose language: ru, en, fr, ...")
+
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    user_language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', {'intl.accept_languages':user_language})
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nQuit browser...")
+    browser.quit()
